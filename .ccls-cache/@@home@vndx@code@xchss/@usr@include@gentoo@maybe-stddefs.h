b7179db9693fd4587bd438382a130c4154bdbd2d
viandoxdev <titouangoulois29@gmail.com>
2025-07-06 18:58:09 +0200

    init

--- .ccls-cache/@@home@vndx@code@xchss/@usr@include@gentoo@maybe-stddefs.h
+++
@@ -1,5 +0,0 @@
-/* __has_include is an extension, but it's fine, because this is only
-for Clang anyway. */
-#if defined __has_include && __has_include (<stdc-predef.h>) && !defined(__GLIBC__)
-# include <stdc-predef.h>
-#endif
